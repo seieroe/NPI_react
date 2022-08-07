499a70874cda5818da0310a58dc839afea57c4a4
Emily Seieroe <e.e.seieroe@gmail.com>
2022-08-07 17:31:04 -0400

    hello world react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { Article, Brand, Cta, Feature, Navbar } from './components'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1 class="title">National Provider Index Lookup</h1>
+        <h2 class="description"> Enter the National Provider Index (NPI) below to find provider details: </h2> 
+
+
+        App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
